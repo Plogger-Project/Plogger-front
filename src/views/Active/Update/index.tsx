8bd665a90f1d51dc9751ce56693d4e52ed37ef12
imwkdrh <imwkdrh7@naver.com>
2024-10-10 09:05:05 +0900

    test commit

---
+++ src/views/Active/Update/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>This is a simple React component.</p>
+      <button>Click Me</button>
+      <p>This is a paragraph.</p>
+      <h2>This is a heading 2</h2>
+      <p>This is a paragraph.</p>
+    </div>
+  )
+}
