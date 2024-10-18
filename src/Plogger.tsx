2864008d349d2dca58f608901d555cc41d9e0be1
kijung <kijung991008@naver.com>
2024-10-18 12:05:14 +0900

    2024.10.18 mainpage

--- src/Plogger.tsx
+++ src/Plogger.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Plogger.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './views/Auth';
+import RecruitPost from './views/Recruit';
+import ActivePost from './views/Active';
+import Main from './views/Main';
 
 function Plogger() {
   return (
-    <Route path='/signup' element={<SignUp />} />
+    <Router>
+      <Routes>
+        <Route path='/main' element={<Main />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/recruit' element={<RecruitPost />} />
+        <Route path='/active' element={<ActivePost />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/views/Main/index.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './style.css';
+
+// component: 메인페이지 컴포넌트 //
+export default function Main() {
+
+  // render: 메인페이지 컴포넌트 렌더링 //
+  return (
+    <div id="main-wrapper">
+      {/* 제거 */}
+      <div className='navigation'>
+        <h1>네비게이션</h1>
+      </div>
+      {/* // 제거 (router 사용하여 빼기) */}
+
+      {/* 이미지 및 버튼 섹션 */}
+      <div className='image-section'>
+        {/* 메인 타이틀 섹션 */}
+        <div className='title-section'>플로거</div>
+        {/* 버튼 섹션 */}
+        <div className='button-section'>
+          <div className='button-left'>지역을 입력해주세요</div>
+          <div className='button-right'>검색</div>
+        </div>
+      </div>
+      {/*컨텐츠 섹션 */}
+      <div id='content-wrapper'>
+        <div className='religion-wrapper'>
+          <div className='religion-text'>지역별 활성도</div>
+          <div className='religion-image'>이미지 구역</div>
+        </div>
+        <div className='popular-wrapper'>
+          <div className='popular-title-box'>
+            <div className='popular-text'>인기글</div>
+            <div className='popular-image'></div>
+          </div>
+          <div className='popular-content'>
+            <div className='popular-content-box'>
+              <div className='popular-content-title'>구인게시판</div>
+              <div className='popular-content-list'>내용</div>
+            </div>
+            <div className='popular-active-content'>
+              <div className='popular-active-title'>활동게시판</div>
+              <div className='popular-active-list'>내용</div>
+            </div>
+          </div>
+        </div>
+        <div className='recent-wrapper'>
+          <div className='recent-title-box'>
+            <div className='recent-text'>최신글</div>
+            <div className='recent-image'></div>
+          </div>
+          <div className='recent-content'>
+            <div className='recent-content-box'>
+              <div className='recent-content-title'>구인게시판</div>
+              <div className='recent-content-list'>내용</div>
+            </div>
+            <div className='recent-active-content'>
+              <div className='recent-active-title'>활동게시판</div>
+              <div className='recent-active-list'>내용</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
