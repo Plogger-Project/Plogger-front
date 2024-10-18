7fcfcef39a296126c340ba30138872d70b084de0
workmakerdaily <sver12@naver.com>
2024-10-18 14:04:44 +0900

    24.10.18 14:00

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Plogger from './Plogger';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NavigationBar from './views/NavigationBar';
-import SignUp from './views/Auth';
-import RecruitPost from './views/Recruit';
-import ActivePost from './views/Active';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,12 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <NavigationBar />
-      <Routes>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/recruit" element={<RecruitPost />} />
-        <Route path="/active" element={<ActivePost />} />
-      </Routes>
+      <Plogger />
     </BrowserRouter>
   </React.StrictMode>
 );
